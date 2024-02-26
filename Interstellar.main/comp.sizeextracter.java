6ee795e432fd14e2dab75e0287ea79cb7872dfa2
480132 <83782172+480132@users.noreply.github.com>
2024-02-26 08:18:04 -0800

    Create comp.sizeextracter.java

---
+++ Interstellar.main/comp.sizeextracter.java
@@ -0,0 +1,17 @@
+if (printed=source)
+  ("The_md")
+    
+</div>
+
+## Deployment 🔽
+
+### Easy Deployment
+
+**Note:** You **cannot** deploy to static web hosts, including Netlify, Cloudflare Pages, and GitHub Pages.
+
+<a target="_blank" href="https://heroku.com/deploy/?template=https://github.com/interstellarnetwork/interstellar">
+<a target="_blank" href="https://app.koyeb.com/deploy?type=git&repository=github.com/interstellarnetwork/interstellar">
+<a target="_blank" href="https://app.cyclic.sh/api/app/deploy/interstellarnetwork/Interstellar"><img alt="Deploy to Cyclic" 
+    <img alt="Deploy to Heroku" src="https://binbashbanana.github.io/deploy-buttons/buttons/remade/heroku.svg"></a>
+[savalge-deplay]
+    <img alt="Deploy to Koyeb" src="https://binbashbanana.github.io/deploy-buttons/buttons/remade/koyeb.svg"></a>
