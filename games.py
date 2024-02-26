ff33c8b7276d45b36797677fd9a937229f53b1f1
480132 <83782172+480132@users.noreply.github.com>
2024-02-25 20:17:41 -0800

    Create games.py

---
+++ games.py
@@ -0,0 +1,23 @@
+in tykner import [fixture_DATA_settings]
+  update{!yml().ymd.}
+bootstrap=https://www.()
+import BOOST_AUTO() :
+  }
+  
+    }  
+    
+AutoWedgeTestFixture()
+	{import pull fixture() {'https://www.nsn_coding.com/pull'}
+   import moudles:
+
+  print("doors")
+  print("Cash-Tycoon")
+  print("Easy-Obby-EARN-RBX")
+  print("
+  print("
+  print("
+  print("
+
+
+
+print("CREDIT : ROBLOX FOUNDATION")
